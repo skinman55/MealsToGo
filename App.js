a9231311a8e53fd95962991e11db4e8178f0758a
Jeremy Skinner <jeremy@o3solution.com>
2023-10-02 17:39:54 -0500

    add tab icons

--- App.js
+++ App.js
@@ -6,12 +6,28 @@ import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeArea } from "./src/components/utility/safe-area.component";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import { theme } from "./src/infrastructure/theme";
 ///const isAndroid = Platform.OS === "android";
 //console.log('StatusBar.currentHeight', );
 
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
 
 const Settings = () => (
   <SafeArea>
@@ -48,7 +64,7 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator screenOptions={createScreenOptions}>
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
