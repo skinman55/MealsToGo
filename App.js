3cf7b5c515a397b4420a55b5d6dfd0926a4f80ff
Jeremy Skinner <jeremy@o3solution.com>
2023-10-02 11:32:46 -0500

    added base structure for restaurant screen and restaurant-info component

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
-import { Searchbar, SafeAreaView } from "react-native-paper";
-
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen.js";
 ///const isAndroid = Platform.OS === "android";
 
 // const [searchQuery, setSearchQuery] = React.useState("");
@@ -12,21 +10,8 @@ import { Searchbar, SafeAreaView } from "react-native-paper";
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, marginTop: StatusBar.currentHeight },
-  search: { padding: 16, backgroundColor: "green" },
-  list: { flex: 1, padding: 16, backgroundColor: "blue" },
-});
